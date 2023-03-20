6ddcb6d4fd41f45181d863f30c4527a097c20b18
Melancholy22 <56004966+Melancholy22@users.noreply.github.com>
2023-03-20 15:26:48 -0700

    Stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ide_art_mobile_app/pages/entry copy.dart';
 import 'package:ide_art_mobile_app/pages/landing_page.dart';
 import 'package:ide_art_mobile_app/pages/sign_up.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
+import 'amplifyconfiguration.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +15,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        //home: SignUp(),
+        home: entryScreen(),
         //home: HomePage(),
-        //home: SignUp(),
-        home: LoginPage(),
+        //home: LandingPage(),
+        //home: LoginPage(),
         theme: ThemeData(primarySwatch: Colors.blue)); //MaterialApp
   }
 }

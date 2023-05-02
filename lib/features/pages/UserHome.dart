00a0f9a1de559febaa0067e0a4e001d63ca75856
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2023-05-01 20:48:51 -0700

    user home update

--- lib/features/pages/UserHome.dart
+++ lib/features/pages/UserHome.dart
@@ -1,3 +1,5 @@
+import 'package:amplify_api/model_queries.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:ide_art_mobile_app/common/utils/colors.dart';
@@ -10,12 +12,17 @@ import 'challengemodel.dart';
 import '../../models/UserPosts.dart';
 
 
-class UserHome extends StatelessWidget {
+class UserHome extends StatefulWidget {
+  const UserHome({super.key});
 
-  final List<UserPosts> _posts = [
-    UserPosts(id:"f68e5ed3-9ee1-4427-ac51-e89ddd567a01", author: "robjb822", title: "THE Billy", description:"THE BRONCO", image:"https://react-art-website-image-storage231705-staging.s3.us-west-1.amazonaws.com/public/images/fbcc9b5b-7734-4bec-a5e2-5a39eb015396BillyBronco.jpg"),
-    UserPosts(id:"2e91a1dc-a8d7-4696-8b47-84c365086d2a", author: "Meloncholy", title: "PIcture", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ", image:"https://static.wikia.nocookie.net/konosuba/images/4/4d/Prev_Chomusuke.png/revision/latest?cb=20161216071437"),
-  ];
+    @override
+  State<UserHome> createState() => _UserHomeState();
+
+}
+
+class _UserHomeState extends State<UserHome>{
+
+  List<UserPosts> _posts = [];
 
   final List<Challenge> _challenges = [
     Challenge(image: "https://static.wikia.nocookie.net/pusheenthecat/images/1/14/Aboutpusheen.gif/revision/latest/scale-to-width-down/325?cb=20140728143155", title: "Pusheen", description: "pushe"),
@@ -25,6 +32,87 @@ class UserHome extends StatelessWidget {
 
   ];
 
+  final controller = ScrollController();
+  final limit = 4;
+  bool hasMore = true;
+  bool isLoading = false;
+  PaginatedResult<UserPosts>? firstPageData;
+
+  @override
+  void initState(){
+    super.initState();
+    initLoad();
+    controller.addListener(() {
+      if(controller.position.maxScrollExtent == controller.offset) {
+        fetch();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  Future initLoad() async {
+    final firstRequest = ModelQueries.list<UserPosts>(
+      UserPosts.classType, 
+      limit: limit,
+      );
+    final firstResult = await Amplify.API.query(request: firstRequest).response;
+    firstPageData = firstResult.data;
+    var items = firstPageData?.items;
+    final List newItems = items!.whereType<UserPosts>().toList();
+    setState(() {
+      _posts.addAll(newItems as Iterable<UserPosts>);
+      safePrint("adding items");
+      safePrint(_posts);
+    });
+  }
+
+  Future fetch() async {
+    if (isLoading) return;
+    isLoading = true;
+
+    firstPageData = await queryPaginatedData();
+    setState(() {
+      hasMore = firstPageData?.hasNextResult ?? false;
+    });
+    if (hasMore == true){
+      var items = firstPageData?.items;
+      final List newItems = items!.whereType<UserPosts>().toList();
+      setState(() {
+        isLoading = false;
+        _posts.addAll(newItems as Iterable<UserPosts>);
+        safePrint("adding items");
+        safePrint(_posts);
+      });
+    }
+  }
+
+  Future<PaginatedResult<UserPosts>?> queryPaginatedData() async {
+    if (firstPageData?.hasNextResult ?? false) {
+      final secondRequest = firstPageData!.requestForNextResult;
+      final secondResult =
+          await Amplify.API.query(request: secondRequest!).response;
+          final secondPageData = secondResult.data;
+      return secondPageData;
+    } else {
+      return firstPageData;
+    }
+  }
+
+  Future refresh() async {
+    setState(() {
+      isLoading = false;
+      hasMore = true;
+      _posts.clear();
+      initLoad();
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,66 +120,85 @@ class UserHome extends StatelessWidget {
     return Scaffold(
       appBar: topBar(),
 
-      body: ListView (
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    ColorizeAnimatedText("Explore Challenges",
-                      textStyle: titleStyle,
-                      colors: ideArtColors,
-                    ),
-                  ],
-                  isRepeatingAnimation: true,
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView (
+              controller: controller,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      ColorizeAnimatedText("Explore Challenges",
+                        textStyle: titleStyle,
+                        colors: ideArtColors,
+                        speed: const Duration(milliseconds: 200),
+                      ),
+                    ],
+                    // isRepeatingAnimation: true,
+                    // repeatForever: true,
+                    // pause: const Duration(milliseconds: 0),
+                  ),
                 ),
-              ),
-              
-              // Challenges
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  child: ListView.builder(
-                    padding: const EdgeInsets.symmetric(horizontal:8.0),
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemCount: _challenges.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index){
-                    return ChallengeTile(
-                      child: _challenges[index],
-                    );
+                
+                // Challenges
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal:8.0),
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: _challenges.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index){
+                      return ChallengeTile(
+                        child: _challenges[index],
+                      );
+                    }),
+                  ),
+      
+            
+                const Padding(
+                  padding: EdgeInsets.only(top:16.0, left:16.0, right:16.0),
+                  child: GradientText(
+                    'Recent Posts',
+                    style: TextStyle(
+                      fontSize: 24, 
+                      fontFamily: 'Poppins', 
+                      fontWeight: FontWeight.bold
+                      ),
+                    gradient: ideArtColor1,
+                  ),
+                ),
+                
+                // UserPosts
+                ListView.builder(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  shrinkWrap: true,
+                  primary: false,
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: _posts.length + 1,
+                  itemBuilder: (context, index){
+                    if(index < _posts.length){
+                    return UserPost(
+                      child: _posts[index],
+                      );
+                     }
+                     else{
+                      return  Padding(
+                        padding: const EdgeInsets.symmetric(vertical:16.0), 
+                        child: Center(
+                          child: hasMore 
+                          ? const CircularProgressIndicator()
+                          : const Text("No more data")
+                        ),
+                      );
+                     }
                   }),
-                ),
-
-          
-              const Padding(
-                padding: EdgeInsets.only(top:16.0, left:16.0, right:16.0),
-                child: GradientText(
-                  'Recent Posts',
-                  style: TextStyle(
-                    fontSize: 24, 
-                    fontFamily: 'Poppins', 
-                    fontWeight: FontWeight.bold
-                    ),
-                  gradient: ideArtColor1,
-                ),
-              ),
-              
-              // UserPosts
-              ListView.builder(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                shrinkWrap: true,
-                primary: false,
-                physics: const ClampingScrollPhysics(),
-                itemCount: _posts.length,
-                itemBuilder: (context, index){
-                  return UserPost(
-                    child: _posts[index],
-                    );
-                }),
-            ],
-          ),
+              ],
+            ),
+      ),
     );
   }
 }
